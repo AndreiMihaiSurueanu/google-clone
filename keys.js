9871ad1993019fb5ad56ff176b54922de4d71ab1
andrei <git1@email.com>
2022-09-22 13:31:34 +0200

    🔴 Let's build Google 2.0 with Tailwind CSS & NEXT.JS!
    (Responsive, SSR React, Pagination)
    * Building the Seach Results
    * Building the Pagination Component

---
+++ keys.js
@@ -0,0 +1,6 @@
+// API Key - https://developer.google.com/custom-search/v1/using_rest
+const API_KEY =
+  "https://developer.google.com/custom-search/v1/api_key";
+
+// Context Key - https://cse.google.com/cse/create/new
+const CONTEXT_KEY = "9867c733a42c0c643";
--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,42 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
+import mockedResponse from "../Response";
 
-function Search() {
+function Search({ results }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      {/* Search Results */}
+      <SearchResults results={results} />
     </div>
   );
 }
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? mockedResponse
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_CUSTOM_SEARCH_API_KEY}&cx=${process.env.NEXT_PUBLIC_GOOGLE_CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
+
+  // After the SERVER has rendered... Pass the results to the client..
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
